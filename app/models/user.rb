6133fb850ffce917a427678241049d1c71e48211
sixokrock7 <sixokrock.7@gmail.com>
2019-01-03 13:43:09 +0900

    Set validates user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :encrypted_password, presence: true, length: { minimum: 6 }, confirmation: true
+  validates :phone_number, presence: true
 end

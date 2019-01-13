9e93d73ec68af3f89c0686589ecf00d071558ba6
sixokrock7 <sixokrock.7@gmail.com>
2019-01-13 17:57:48 +0900

    Add test authorization

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -290,4 +290,7 @@ Devise.setup do |config|
   config.omniauth :google_oauth2,
                   Rails.application.secrets.google_client_id,
                   Rails.application.secrets.google_client_secret
+  config.omniauth :facebook,
+                  Rails.application.secrets.facebook_client_id,
+                  Rails.application.secrets.facebook_client_secret
 end

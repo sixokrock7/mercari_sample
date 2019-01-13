f9dc07b85938e442ff6a87a3f7a9ed12462b2ebc
sixokrock7 <sixokrock.7@gmail.com>
2019-01-13 17:57:03 +0900

    Edit pass in omniauth_callbacks_controller

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -14,7 +14,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       sign_in_and_redirect @user. event: :authentication
     else
       session["devise.#{provider}_data"] = request.env["omniauth.auth"].expect("extra")
-      redirect_to new_user_session_url
+      redirect_to sign_up_registration_path
     end
   end
 

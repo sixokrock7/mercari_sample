97ae332ec2428d08f4b61a5400e7667353707998
sixokrock7 <sixokrock.7@gmail.com>
2019-01-24 13:23:23 +0900

    Create controller session

---
+++ app/controllers/users/sessions_controller.rb
@@ -0,0 +1,14 @@
+class Users::SessionsController < Devise::SessionsController
+  before_action :authenticate_user!
+
+  def new
+    @resource = User.new
+  end
+
+  protected
+
+  def after_sign_up_path_for(resource)
+    new_user_session_path
+  end
+
+end

26daecc9744a1c525752252d646d38669f6d5cc7
sixokrock7 <sixokrock.7@gmail.com>
2019-02-05 11:16:27 +0900

    Edit routing

--- config/routes.rb
+++ config/routes.rb
@@ -6,18 +6,24 @@ Rails.application.routes.draw do
     omniauth_callbacks: "users/omniauth_callbacks"
   }, skip: [:sessions]
   devise_scope :user do
-    get "users/sign_in" => "users/sessions#new"
+    get "users/sign_in" => "users/sessions#new", as: :new_user_session
+    post "user/sign_in" => "users/sessions#create", as: :user_session
     get "users/sign_in/identification" => "users/sessions#identification"
+    match 'users/logout' => 'users/sessions#destroy', as: :destroy_user_session, via: Devise.mappings[:user].sign_out_via
+    get 'users/logout' => 'users/sessions#destroy'
     get "/sign_up/basic_info" => "users/registrations#basic_info"
-    get "/sign_up/sms_confirmation" => "users/registrations#sms_confirmation"
-    get "/sign_up/address" => "users/registrations#address"
-    get "/sign_up/card" => "users/registrations#card"
-    get "/sign_up/completed" => "users/registrations#create"
+    post "/sign_up/sms_confirmation" => "users/registrations#sms_confirmation"
+    post "/sign_up/address" => "users/registrations#address"
+    post "/sign_up/card" => "users/registrations#card"
+    post "/sign_up/completed" => "users/registrations#create"
     get "sign_up/done" => "users/registrations#done"
   end
 
   root "tops#index"
   resources :tops, only: [:index]
-  resources :users, only:[:create, :edit, :show]
+  resources :users, only:[:create, :edit, :show, :destroy] do
+    resources :addresses do
+    end
+  end
   resources :mypages, controller: :users, only: [:show]
 end
